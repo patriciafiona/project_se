9eb7c5ee6bec394c4d97f359694c0bd320dd3f16
PatriciaFiona <patriciafiona3@gmail.com>
2019-05-09 12:59:11 +0700

    Tambahan index.blade.php untuk pasien tetap

---
+++ resources/views/D_Pasien/index.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+    @include('layouts.headers.cards')
+
+    <br/><br/><br/>
+
+    <div class="container-fluid mt--7">
+        <div class="row">
+            <div class="col-xl-12">
+                <div class="card shadow">
+                    <div class="card-header bg-transparent">
+                        <div class="row align-items-center">
+                            <div class="col">
+                                <h2 class="mb-0">Daftar Pasien Tetap</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <!--Insert CODE HERE-->
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        @include('layouts.footers.auth')
+    </div>
+@endsection
+
+@push('js')
+    <script src="{{ asset('argon') }}/vendor/chart.js/dist/Chart.min.js"></script>
+    <script src="{{ asset('argon') }}/vendor/chart.js/dist/Chart.extension.js"></script>
+@endpush
